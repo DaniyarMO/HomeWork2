9b2d039d7413bba7b791b7acea9ebf95ca37c355
DaniyarMO <162583365+DaniyarMO@users.noreply.github.com>
2024-03-13 12:04:04 +0500

    Add files via upload

---
+++ bornyearforewer..py
@@ -0,0 +1,8 @@
+
+while True:
+    age = int(input("Укажите год рождения А.С.Пушкина: "))
+    if age == 1799:
+        break
+    print("Не верно")
+print("Верно!")
+
---
+++ variables.py
@@ -0,0 +1,9 @@
+food = "яблоко"
+price = 5.25
+best_date = 10
+in_stock = True
+print(type(food))
+print(type(price))
+print(type(best_date))
+print(type(in_stock))
+
